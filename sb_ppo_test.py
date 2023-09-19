589ee808ffb287dea2bdfc1b0ac437681db4ff63
Quamsy <36869195+QuamCode@users.noreply.github.com>
2023-09-19 14:02:40 +0800

    Initial PPO implementation using SB3

--- sb_ppo_test.py
+++ sb_ppo_test.py
@@ -1,56 +1,39 @@
-# Stable Baselines3 compatible wrapper for Super Mario Bros
-
 import gym
 import numpy as np
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-from gym.wrappers import GrayScaleObservation, ResizeObservation
+from gym.wrappers import GrayScaleObservation
+from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
-import matplotlib.pyplot as plt
 
-# To get it to run needed to remove seed and options from reset, which I guess are added by DummyVecEnv
+# Wrapper to remove seed and options from reset
 class RemoveSeedWrapper(gym.Wrapper):
     def reset(self, **kwargs):
         kwargs.pop('seed', None)
         kwargs.pop('options', None)
         return super().reset(**kwargs)
 
-# Support for grayscale and frame stacking
-class SqueezeObservation(gym.ObservationWrapper):
-    def observation(self, observation):
-        return np.squeeze(observation)
+# Environment setup
+test_env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
+test_env = JoypadSpace(test_env, SIMPLE_MOVEMENT)
+test_env = RemoveSeedWrapper(test_env)
+test_env = GrayScaleObservation(test_env)
+# Vectorized environment for Stable Baselines3
+test_env = DummyVecEnv([lambda: test_env])
 
-env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
-env = RemoveSeedWrapper(env)
-env = GrayScaleObservation(env)
-env = ResizeObservation(env, (84, 84))
-env = SqueezeObservation(env)
+# Load the best model
+best_model = PPO.load("./logs/best_model/best_model")
 
-# env = gym_super_mario_bros.make('SuperMarioBros-v0')
-# For Stable Baselines3 we need to use a vectorized environment
-env = DummyVecEnv([lambda: env])
+# Reset the environment
+state = test_env.reset()
 
-state = env.reset()
-# print(state.shape)
-#
-frame_stack = np.zeros((1, state.shape[1], state.shape[2], 10))
-# fRAme stacking manually as it doesn't seem to work with the stable baselines wrapper
-
-for step in range(100):
-    action = [env.action_space.sample()]
-    state, reward, done, info = env.step(action)
-    frame_stack = np.roll(frame_stack, shift=-1, axis=3)
-    frame_stack[:, :, :, -1] = state[:, :, :]
-
+# Run the best model
+for _ in range(1000):
+    action, _ = best_model.predict(state)
+    state, reward, done, info = test_env.step(action)
+    test_env.render()
     if done.any():
-        state = env.reset()
-        frame_stack = np.zeros((1, state.shape[1], state.shape[2], 10))
+        state = test_env.reset()
 
-# Visualize the frame stack
-plt.figure(figsize=(20, 10))
-for i in range(10):
-    plt.subplot(2, 5, i + 1)
-    plt.imshow(frame_stack[0, :, :, i], cmap='gray')
-    plt.title(f"Frame {i + 1}")
-plt.show()
+# Close the environment
+test_env.close()
